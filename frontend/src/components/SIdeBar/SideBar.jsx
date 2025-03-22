7f6b37492fcc3a8cfc1eb355df6018546c8aa452
rahmonovsaydullo <saydullo779@gmail.com>
2025-03-22 15:37:10 +0500

    I completed SideBar component

--- frontend/src/components/SIdeBar/SideBar.jsx
+++ frontend/src/components/SIdeBar/SideBar.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { 
+  faBell as faBellRegular, 
+  faBookmark as faBookmarkRegular, 
+  faEnvelope as faEnvelopeRegular, 
+  faUser as faUserRegular 
+} from '@fortawesome/free-regular-svg-icons';
+import { faEllipsisH, faHashtag, faHouseChimney, faList } from '@fortawesome/free-solid-svg-icons';
 
 const SideBar = () => {
+  let icons = [
+    { icon: faHouseChimney, title: "Home", color: "rgb(29, 161, 242)" },
+    { icon: faHashtag, title: "Explore", color: "black" },
+    { icon: faBellRegular, title: "Notification", color: "black" },  
+    { icon: faEnvelopeRegular, title: "Message", color: "black" },   
+    { icon: faBookmarkRegular, title: "Bookmarks", color: "black" }, 
+    { icon: faList, title: "Lists", color: "black" },
+    { icon: faUserRegular, title: "Profile", color: "black" },       
+    { icon: faEllipsisH, title: "More", color: "black" },
+  ];
+
   return (
-    <div>
-      <FontAwesomeIcon className='text-2xl' icon={faTwitter} style={{ color: 'rgb(29, 161, 242)' }} />
+    <div className='flex flex-col items-start justify-center mt-2 ms-28 gap-6 '>
+      <FontAwesomeIcon className='text-3xl' icon={faTwitter} style={{ color: 'rgb(29, 161, 242)' }} />
+      {icons.map((icon, index) => (
+        <div key={index} className='flex align-middle gap-3 text-lg cursor-pointer'>
+          <div>
+            <FontAwesomeIcon icon={icon.icon} style={{ color: icon.color }} className='text-2xl' />
+          </div>
+          <p className='font-semibold' style={{ color: icon.color }}>{icon.title}</p>
+        </div>
+      ))}
+     <button className='btn  text-white flex justify-center align-middle py-3 px-20 rounded-full ' style={{backgroundColor: "rgb(29, 161, 242"}}>Tweet</button>
     </div>
   );
 };
