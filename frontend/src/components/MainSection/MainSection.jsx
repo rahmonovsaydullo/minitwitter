05c2bb132a6bcdb614258cd9c60423e45a0514bf
rahmonovsaydullo <saydullo779@gmail.com>
2025-03-22 15:41:26 +0500

    I created Main section

--- frontend/src/components/MainSection/MainSection.jsx
+++ frontend/src/components/MainSection/MainSection.jsx
@@ -1,9 +1,11 @@
+import { faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const MainSection = () => {
   return (
     <div>
-      
+      <FontAwesomeIcon icon={faWandMagicSparkles} style={{ color: 'rgb(29, 161, 242)', fontSize: '24px' }} />
     </div>
   )
 }
