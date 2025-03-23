1597efc226887501bb468033e1f6926410b0934a
rahmonovsaydullo <saydullo779@gmail.com>
2025-03-23 16:00:39 +0500

    I completed userRoutes.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,11 @@
 const {Pool} = require("pg")
 
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "twitter",
+    password: "12345qwerty",
+    port: 5432
+})
 
+module.exports = pool;
