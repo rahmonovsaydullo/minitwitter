80c0805667933886e62e0543965672fd11dd601f
rahmonovsaydullo <saydullo779@gmail.com>
2025-04-02 10:45:56 +0500

    I added date-fns

--- frontend/src/components/Posts/Posts.jsx
+++ frontend/src/components/Posts/Posts.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpFromBracket as faArrowUpFromBracketRegular, faComment, faHeart } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
-
+import { formatDistanceToNow } from "date-fns";
 
 const Posts = () => {
 
@@ -10,7 +10,7 @@ const Posts = () => {
 
     const fetchPosts = async () => {
         try {
-            const response = await axios   
+            const response = await axios
                 .get('http://localhost:3000/posts')
 
             console.log(response.data);
@@ -27,19 +27,21 @@ const Posts = () => {
     }, [])
 
     return (
-        <div className='w-full border bg-gray-100 '>
+        <div className='w-full border  bg-gray-100 '>
             {
                 posts.map((post) => (
-                    <div key={post.id}>
-                            <div>
-                                <p>{post.name}</p>
-                                <p>{post.username}</p>
-                                <p>{formatDistanceToNow(new Date(post.created_at), { addSuffix: true })}</p>
-
-                            </div>
-                            <p>{post.text}</p>
-                        <img src={post.post_img} alt="" />
-                        <div className='flex justify-around'> <FontAwesomeIcon icon={faComment} /> <FontAwesomeIcon icon={faArrowUpFromBracketRegular} /> <FontAwesomeIcon icon={faHeart} style={{ color: "red" }} /></div>
+                    <div className='px-3 py-3' key={post.id}>
+                        <div className='flex justify-between'>
+                            <p className='text-blue-500'>@{post.username}</p>
+                            <p>{formatDistanceToNow(new Date(post.created_at), { addSuffix: true })}</p>
+                        </div>
+                        <p className='text-2xl'>{post.text}</p>
+                        {/* <img src={post.post_img} alt="" /> */}
+                        <div className='flex justify-around pt-2 '> 
+                            <FontAwesomeIcon icon={faComment}  className='cursor-pointer'/>
+                             <FontAwesomeIcon icon={faArrowUpFromBracketRegular}  className='cursor-pointer'/>
+                              <FontAwesomeIcon icon={faHeart} style={{ color: "red" }}  className='cursor-pointer'/>
+                        </div>
                     </div>
                 ))
             }
